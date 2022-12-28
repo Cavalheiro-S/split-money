0e4a772c80d9129079f28ef83df3176c3b1d6937
Lucas Cavalheiro <luccribeiro53@gmail.com>
2022-12-28 17:32:59 -0300

    fix: fix .env files to Vite build

--- src/Utils/firebase.ts
+++ src/Utils/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID,
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 export const app = initializeApp(firebaseConfig);
