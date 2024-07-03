16da0ea252770e32e9e4817d3a7b2af34b8f8a77
Lucas Cavalheiro <luccribeiro53@gmail.com>
2024-07-03 08:55:56 -0300

    config: log to verif webpack config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,13 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "dist",
   webpack: (config) => {
+    console.log('Configuração atual do Webpack:', config);
     if (config.resolve.plugins) {
       config.resolve.plugins.push(new TsconfigPathsPlugin());
     } else {
       config.resolve.plugins = [new TsconfigPathsPlugin()];
     }
-
+    console.log('Configuração do Webpack após adicionar TsconfigPathsPlugin:', config.resolve.plugins);
     return config;
   }
 }
