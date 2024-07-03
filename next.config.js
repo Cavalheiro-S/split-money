17c38e137343421c047f750c3a130b5150f52aea
Lucas Cavalheiro <luccribeiro53@gmail.com>
2024-07-03 08:23:37 -0300

    config: suport to personalized paths to webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "dist"
+  distDir: "dist",
+  webpack: (config) => {
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(new TsconfigPathsPlugin());
+    } else {
+      config.resolve.plugins = [new TsconfigPathsPlugin()];
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Button, Modal as ModalAnt } from "antd";
 import React from 'react';
 
