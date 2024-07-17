31195e06612dae54d05117739d8742175fdce7d0
Lucas Cavalheiro <luccribeiro53@gmail.com>
2024-07-17 15:32:50 -0300

    config: adjust env variable

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
     }
     config.resolve.plugins.push(new TsconfigPathsPlugin());
     return config;
+  },
+  env: {
+    NEXTAUTH_SECRET: "testeSecret",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || `https://${process.env.VERCEL_URL}`
   }
 }
 
